7558835eae805ec8cddc0092f2943e09e2a66977
Tommi <t.cambursano@gmail.com>
2026-02-14 13:14:53 +0100

    final

--- constants.ts
+++ constants.ts
@@ -35,39 +35,37 @@ export const MEMORIES: Memory[] = [
     imageUrl: "/images/12.png"
   },
   {
+    id: '6',
+    title: "Suuushi",
+    description: "Bed close to you, sushi close to you",
+    date: "2025-07-14",
+    location: { lat: 41.144188, lng: -8.605255 },
+    imageUrl: "/images/15.jpg"
+  },
+  {
     id: '5',
     title: "Primo aperitivo ;)",
     description: "Moments get better with you",
     date: "2025-07-18",
     location: { lat: 41.143078, lng: -8.614786 },
-    imageUrl: "/images/13.png"
-  },
-  {
-    id: '6',
-    title: "Suuushi",
-    description: "Bed close to you, sushi close to you",
-    date: "2025-07-14",
-    location: { lat: 41.14400949343178, lng: -8.609892797698134 },
-    imageUrl: "/images/2.jpg"
+    imageUrl: "/images/13.jpg"
   },
   {
     id: '7',
-    title: "Falling hard for you ",
+    title: "Falling hard for you",
     description: "",
-    date: "2025-07-14",
+    date: "2025-07-21",
     location: { lat: 41.137742, lng: -8.609224 },
     imageUrl: "/images/9.jpg"
   },
-
-  {
+    {
     id: '8',
-    title: "Sleepy girl again",
-    description: "I was kinda unconfy, but I could not desire a better place to be",
+    title: "Super hard",
+    description: "",
     date: "2025-07-24",
-    location: { lat: 39.971353, lng: -8.641322 },
-    imageUrl: "/images/3.jpg"
+    location: { lat: 38.795514, lng: -9.394983 },
+    imageUrl: "/images/10.jpg"
   },
-
   {
     id: '9',
     title: "Sleepy girl again",
@@ -75,5 +73,220 @@ export const MEMORIES: Memory[] = [
     date: "2025-07-24",
     location: { lat: 39.971353, lng: -8.641322 },
     imageUrl: "/images/3.jpg"
-  }
+  },
+    {
+    id: '10',
+    title: "Always late",
+    description: "",
+    date: "2025-07-24",
+    location: { lat: 41.244793, lng: -8.678496 },
+    imageUrl: "/images/16.jpg"
+  },
+      {
+    id: '11',
+    title: "Videocalling",
+    description: "Con un po' di stress",
+    date: "2025-08-02",
+    location: { lat: 41.14392861745795, lng: -8.6102193708928 },
+    imageUrl: "/images/11.jpg"
+  },
+      {
+    id: '12',
+    title: "Summer forever",
+    description: "",
+    date: "2025-08-13",
+    location: { lat: 43.926285, lng: 8.114966 },
+    imageUrl: "/images/17.jpg"
+  },
+    {
+    id: '13',
+    title: "Summer forever",
+    description: "No more photos, just memories",
+    date: "2025-08-15",
+    location: { lat: 45.05525890943701, lng: 7.688464713732149 },
+    imageUrl: "/images/18.jpg"
+  },
+      {
+    id: '14',
+    title: "Far away ;(",
+    description: "",
+    date: "2025-09-01",
+    location: { lat: 45.131670071186186, lng: 7.772529109861884 },
+    imageUrl: "/images/19.jpg"
+  },
+        {
+    id: '15',
+    title: "But back togheter soon",
+    description: "Coming to my giiiirl",
+    date: "2025-11-09",
+    location: { lat: 51.88591775114545, lng: 0.24314137057868165 },
+    imageUrl: "/images/20.jpg"
+  },
+    {
+    id: '16',
+    title: "In AMERICA!!!",
+    description: "aaaaaa",
+    date: "2025-11-11",
+    location: { lat: 28.51946682814318, lng: -81.37492533230227 },
+    imageUrl: "/images/21.jpg"
+  },
+      {
+    id: '17',
+    title: "In AMERICA!!!",
+    description: "Together",
+    date: "2025-11-12",
+    location: { lat: 27.88627516882561, lng: -82.54152227249084 },
+    imageUrl: "/images/22.jpg"
+  },
+        {
+    id: '18',
+    title: "All together",
+    description: "Io te e i nostri cani :)",
+    date: "2025-11-13",
+    location: { lat: 27.766484744009155, lng: -82.6399219568046 },
+    imageUrl: "/images/23.jpg"
+  },
+        {
+    id: '19',
+    title: "All together",
+    description: "JD!!!! Gigettttto",
+    date: "2025-11-19",
+    location: { lat: 27.766484744009155, lng: -82.6399219568046 },
+    imageUrl: "/images/24.jpg"
+  },
+      {
+    id: '20',
+    title: "Picnic",
+    description: "Dating uuuuu ",
+    date: "2025-11-24",
+    location: { lat: 27.788984109767664, lng: -82.64237011208564 },
+    imageUrl: "/images/25.jpg",
+  },
+        {
+    id: '21',
+    title: "First Chipoltle",
+    description: "",
+    date: "2025-11-24",
+    location: { lat: 27.766484744009155, lng: -82.6399219568046 },
+    imageUrl: "/images/26.jpg",
+  },
+        {
+    id: '22',
+    title: "Walking dogs",
+    description: "",
+    date: "2025-11-25",
+    location: { lat: 27.766713540088407, lng: -82.64160030802947 },
+    imageUrl: "/images/27.jpg",
+  },
+          {
+    id: '23',
+    title: "And dating",
+    description: "On a boat!!!! With dolphine!!!!",
+    date: "2025-11-25",
+    location: { lat: 27.777114502326164, lng: -82.6187598992208 },
+    imageUrl: "/images/32.jpg",
+  },
+        {
+    id: '23',
+    title: "Ballando con le stelle",
+    description: "",
+    date: "2025-11-26",
+    location: { lat: 27.848742707430926, lng: -82.67564838420327 },
+    imageUrl: "/images/28.jpg",
+  },
+          {
+    id: '24',
+    title: "First thanksgiving of my life!!!",
+    description: "With your family :)))",
+    date: "2025-11-27",
+    location: { lat: 28.530463134062092, lng: -81.56649817937485 },
+    imageUrl: "/images/29.jpg",
+  },
+          {
+    id: '25',
+    title: "Some more date with u!",
+    description: "",
+    date: "2025-12-04",
+    location: { lat: 27.773281042219892, lng: -82.63293755281467 },
+    imageUrl: "/images/30.jpg",
+  },
+    {
+    id: '25',
+    title: "JD again",
+    description: "",
+    date: "2025-12-04",
+    location: { lat: 27.766484744009155, lng: -82.6399219568046 },
+    imageUrl: "/images/35.jpg",
+  },
+            {
+    id: '26',
+    title: "And decorating the house",
+    description: "",
+    date: "2025-12-05",
+    location: { lat: 27.766484744009155, lng: -82.6399219568046 },
+    imageUrl: "/images/31.jpg",
+  },
+              {
+    id: '27',
+    title: "Just kissing",
+    description: "",
+    date: "2025-12-11",
+    location: { lat: 27.788673776956625, lng: -82.64217747846531 },
+    imageUrl: "/images/33.jpg",
+  },
+              {
+    id: '28',
+    title: "And ejoying ur company",
+    description: "",
+    date: "2025-12-11",
+    location: { lat: 27.775459518295072, lng: -82.63214106688599 },
+    imageUrl: "/images/34.jpg",
+  },
+                {
+    id: '29',
+    title: "Last chipotle",
+    description: "",
+    date: "2025-12-12",
+    location: { lat: 27.78014640084682, lng: -82.63892021137042 },
+    imageUrl: "/images/36.jpg",
+  },
+     {
+    id: '30',
+    title: "<3",
+    description: "Diving in u",
+    date: "2025-12-12",
+    location: { lat: 27.766484744009155, lng: -82.6399219568046 },
+    imageUrl: "/images/38.jpg",
+  },
+      {
+    id: '31',
+    title: "<3",
+    description: "",
+    date: "2025-12-12",
+    location: { lat: 27.942362167672105, lng: -82.46670341100322 },
+    imageUrl: "/images/37.jpg",
+  },
+        {
+    id: '32',
+    title: "<3",
+    description: "And also if we are far away and things get hard",
+    date: "2025-12-26",
+    location: { lat: 39.01664656882862, lng: -45.46348572792339 },
+    imageUrl: "/images/39.png",
+  },
+          {
+    id: '33',
+    title: "<3",
+    description: "And I am missing u so bad",
+    date: "2025-12-30",
+    location: { lat: 39.01664656882862, lng: -45.46348572792339 },
+    imageUrl: "/images/40.png",
+  },        {
+    id: '34',
+    title: "<3",
+    description: "We still never lose laughing",
+    date: "2026-01-13",
+    location: { lat: 27.942362167672105, lng: -45.46348572792339 },
+    imageUrl: "/images/41.png",
+  },
 ];
